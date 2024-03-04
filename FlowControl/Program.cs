77941d7071b9703373782e895b94d2005c483a3e
Jojaah12 <david.aljojah@gmail.com>
2024-03-04 19:41:21 +0100

    Implemented menu options to repeat user input ten times and determine price category based on user age.

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -11,6 +11,7 @@
                 Console.WriteLine("Main Menu");
                 Console.WriteLine("1. Determine Price Category");
                 Console.WriteLine("2. Repeat User Input Ten Times");
+                Console.WriteLine("3. Extract Third Word");
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice: ");
 
@@ -22,7 +23,10 @@
                         DeterminePriceCategory();
                         break;
                     case "2":
-                        RepeatUserInputTenTimes();
+                        RepeatInputTenTimes();
+                        break;
+                    case "3":
+                        ExtractThirdWord();
                         break;
                     case "0":
                         programRunning = false;
@@ -31,6 +35,23 @@
                         Console.WriteLine("Invalid input. Please try again.");
                         break;
                 }
+            }
+        }
+
+        static void ExtractThirdWord()
+        {
+            Console.Write("Enter a sentence with at least 3 words: ");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+            if (words.Length >= 3)
+            {
+                string thirdWord = words[2];
+                Console.WriteLine("Third Word: " + thirdWord);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a sentence with at least 3 words.");
             }
         }
 
@@ -58,9 +79,9 @@
             }
         }
 
-        static void RepeatUserInputTenTimes()
+        static void RepeatInputTenTimes()
         {
-            Console.Write("Enter some text: ");
+            Console.Write("Enter text to repeat ten times: ");
             string input = Console.ReadLine();
 
             for (int i = 1; i <= 10; i++)
